f9803cfae2fc970eab8dc8f9df0230096c6a295f
Onyxmoises <mmorenor2003@alumno.ipn.mx>
2022-04-01 02:03:49 -0600

    Solucionado

--- 08Rompecabezas/js/rompecabezas.js
+++ 08Rompecabezas/js/rompecabezas.js
@@ -1,51 +1,51 @@
 // obtener todas las piezas
 var piezas = document.getElementsByClassName('movil')
 //redimencionar las piezas
-var tamWidth = [134, 192, 134, 163, 134, 163, 134, 192, 134];
-var tamHeigth = [163, 134, 163, 134, 192, 134, 163, 134, 163];
+var tamWidth = [134,192,134,163,134,163,134,192,134];
+var tamHeigth = [163,134,163,134,192,134,163,134,163];
 for(var i = 0; i < piezas.length; i++){
     // enviar el atributo de w h
     piezas[i].setAttribute("width", tamWidth[i]);
     piezas[i].setAttribute("height", tamHeigth[i]);
-    piezas[i].setAttribute("x", Math.floor(Math.random()*10)+i);
-    piezas[i].setAttribute("y", Math.floor(Math.random()*400)+i);
+    piezas[i].setAttribute("x", Math.floor(Math.random()*10)+1);
+    piezas[i].setAttribute("y", Math.floor(Math.random()*409)+1);
     // que se pueda mover
     piezas[i].setAttribute("onmousedown", "seleccionarElemento(evt)");
 }
-var elementoSeleccionado = 0;
+var elementSelect = 0;
 var currentX = 0;
 var currentY = 0;
 var currentPosX = 0;
 var currentPosY = 0;
 function seleccionarElemento(evt){
-    elementoSeleccionado = reordenar(evt)
+    elementSelect = reordenar(evt)
     currentX = evt.clientX;
     currentY = evt.clientY;
-    currentPosX = parseFloat(elementoSeleccionado.getAttribute("x"));
-    currentPosY = parseFloat(elementoSeleccionado.getAttribute("y"));
+    currentPosx = parseFloat(elementSelect.getAttribute("x"));
+    currentPosy = parseFloat(elementSelect.getAttribute("y"));
     //ahora si debe de existri un movimiento
-    elementoSeleccionado.setAttribute("onmousemove", "moverElemento(evt)");
+    elementSelect.setAttribute("onmousemove", "moverElemento(evt)");
 }
 function moverElemento(evt){
-    var dx = evt.clientX - currentPosX;
-    var dy = evt.clientY - currentPosY;
-    currentPosX = currentPosX + dx
-    currentPosY = currentPosY + dy
-    elementoSeleccionado.setAttribute("x", currentPosX);
-    elementoSeleccionado.setAttribute("y", currentPosY);
-    currentPosX = evt.clientX;
-    currentPosY = evt.clientY;
-    elementoSeleccionado.setAttribute("onmouseout", "deseleccionarElemento(evt)");
-	elementoSeleccionado.setAttribute("onmouseup", "deseleccionarElemento(evt)");
+    var dx = evt.clientX - currentX;
+    var dy = evt.clientY - currentY;
+    currentPosx = currentPosx + dx
+    currentPosy = currentPosy + dy
+    elementSelect.setAttribute("x", currentPosx);
+    elementSelect.setAttribute("y", currentPosy);
+    currentX = evt.clientX;
+    currentY = evt.clientY;
+    elementSelect.setAttribute("onmouseout", "deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup", "deseleccionarElemento(evt)");
 	iman();
 }
 function deseleccionarElemento(evt){
     testing();
-    if(elementoSeleccionado != 0){
-        elementoSeleccionado.removeAttribute("onmousemove");
-        elementoSeleccionado.removeAttribute("onmouseout");
-        elementoSeleccionado.removeAttribute("onmouseup");
-        elementoSeleccionado = 0;
+    if(elementSelect != 0){
+        elementSelect.removeAttribute("onmousemove");
+        elementSelect.removeAttribute("onmouseout");
+        elementSelect.removeAttribute("onmouseup");
+        elementSelect = 0;
     }
 }
 var entorno = document.getElementById('entorno');
@@ -57,13 +57,13 @@ function reordenar(evt){
     entorno.appendChild(clone)
     return entorno.lastChild.firstChild;
 }
-var originX = [200, 304, 466, 200, 333, 437, 200, 304, 466];
-var originY = [100, 100, 100, 233, 204, 233, 337, 366, 337];
+var originX = [200,304,466,200,333,437,200,304,466];
+var originY = [100,100,100,233,204,233,337,366,337];
 function iman(){
     for(var i = 0; i < piezas.length; i++){
-        if(Math.abs(currentPosX - originX[i]) < 15 && Math.abs(currentPosY - originY[i]) < 15){
-            elementoSeleccionado.setAttribute("x", originX)
-            elementoSeleccionado.setAttribute("y", originY)
+        if(Math.abs(currentPosx-originX[i]) < 15 && Math.abs(currentPosy-originY[i]) < 15){
+            elementSelect.setAttribute("x", originX[i]);
+            elementSelect.setAttribute("y", originY[i]);
         }
     }
 }
